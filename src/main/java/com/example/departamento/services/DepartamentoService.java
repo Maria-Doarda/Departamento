b44cda5a38f913960e021349d07868bd87c46e03
Maria <dudahpereira13@gmail.com>
2026-03-24 10:38:33 -0300

    service criado

---
+++ src/main/java/com/example/departamento/services/DepartamentoService.java
@@ -0,0 +1,28 @@
+package com.example.departamento.services;
+
+import com.example.departamento.models.DepartamentoModel;
+import com.example.departamento.repositories.DepartamentoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class DepartamentoService {
+
+    @Autowired
+    private DepartamentoRepository departamentoRepository;
+
+    public DepartamentoModel criarDepartamento(DepartamentoModel departamentoModel){
+        return departamentoRepository.save(departamentoModel);
+    }
+
+    public List<DepartamentoModel> findAll(){
+        return departamentoRepository.findAll();
+    }
+
+    public Optional<DepartamentoModel> buscarId(Long id){
+        return departamentoRepository.findById(id);
+    }
+}
