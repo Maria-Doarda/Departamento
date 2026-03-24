ea01abc8cef2dca98f31efffbf858e119d8efa9d
Maria <dudahpereira13@gmail.com>
2026-03-24 10:33:29 -0300

    repository criado

---
+++ src/main/java/com/example/departamento/repositories/DepartamentoRepository.java
@@ -0,0 +1,7 @@
+package com.example.departamento.repositories;
+
+import com.example.departamento.models.DepartamentoModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DepartamentoRepository extends JpaRepository<DepartamentoModel, Long> {
+}
